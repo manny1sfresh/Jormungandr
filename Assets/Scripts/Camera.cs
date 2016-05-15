42236604f6f89a1d75030b8167b7a88af1e7f90e
manny1sfresh <mesa.gamecoder@gmail.com>
2016-05-15 16:59:38 -0400

    Tank
    Added Tank

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour
+{
+
+    public GameObject Player;
+
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + offset;
+    }
+}
